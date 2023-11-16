4df30d6e644cec3144e3933b75058c6d9387d239
deborah Turner <debturner1964@gmail.com>
2023-11-16 14:59:36 -0500

    spelling corrections made

--- SwiftUIStoryTemplate/Chapter3/Chapter3View.swift
+++ SwiftUIStoryTemplate/Chapter3/Chapter3View.swift
@@ -7,19 +7,19 @@ struct Chapter3View: View {
 
     var body: some View {
         VStack{
-            Text("Kassmia woke up startled")
-            Text("Kassmia was hungry ")
-            Text("Kassmia went downtown to the food trucks")
-            Text("Kass mia walked past the statue in campas marshes  and touched it")
-            Text ("as Kassmia touched it she was immeiataly telewported ")
-            Text("Kassmia noticed shee was in front of a sdchool ")
-            Text("Kassmia heard crying  she saw a young child looking distressed")
-            Text("Kassmia said can i heelp")
+            Text("Kassima woke up startled")
+            Text("Kassima was hungry ")
+            Text("Kassima went downtown to the food trucks")
+            Text("Kassima walked past the statue in Campus Martius  and touched it")
+            Text ("as Kassima touched it she was immeiataly teleported ")
+            Text("Kassima noticed she was in front of a school ")
+            Text("Kassima heard crying  she saw a young child looking distressed")
+            Text("Kassima said can i help")
             Text("the child said yes i forgot my lunch")
-            Text("Kassmia said you are in luck i just bought lunch you can havd")
-            Text("Kassmia said by the way what year is it ")
-            Text("the young child said  it is 1869  the firstime black people were enrolled in American shools")
-            Text("Kassmia and the young child talked and afterqards dhewas immediatly transportted back to her time")
+            Text("Kassima said you are in luck I just bought lunch you can have")
+            Text("Kassima said by the way what year is it ")
+            Text("the young child said  it is 1869  the firstime black people were enrolled in American schools")
+            Text("Kassima and the young child talked and afterwards she was immediately teleported back to her time")
     }
         
     }
