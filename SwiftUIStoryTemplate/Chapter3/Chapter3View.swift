3219008618623c9c55a5f5a7c4e91cb15f8d08a6
Antonio Gregory <antonio.dgregory@gmail.com>
2023-11-16 14:36:43 -0500

    updates 2

--- SwiftUIStoryTemplate/Chapter3/Chapter3View.swift
+++ SwiftUIStoryTemplate/Chapter3/Chapter3View.swift
@@ -1,8 +1,27 @@
 import SwiftUI
 
 struct Chapter3View: View {
+    var isHungry:Bool = true
+    var num: Int = 23
+
+
     var body: some View {
-        Text("Chapter 3")
+        VStack{
+            Text("Kassmia woke up startled")
+            Text("Kassmia was hungry ")
+            Text("Kassmia went downtown to the food trucks")
+            Text("Kass mia walked past the statue in campas marshes  and touched it")
+            Text ("as Kassmia touched it she was immeiataly telewported ")
+            Text("Kassmia noticed shee was in front of a sdchool ")
+            Text("Kassmia heard crying  she saw a young child looking distressed")
+            Text("Kassmia said can i heelp")
+            Text("the child said yes i forgot my lunch")
+            Text("Kassmia said you are in luck i just bought lunch you can havd")
+            Text("Kassmia said by the way what year is it ")
+            Text("the young child said  it is 1869  the firstime black people were enrolled in American shools")
+            Text("Kassmia and the young child talked and afterqards dhewas immediatly transportted back to her time")
+    }
+        
     }
 }
 
--- SwiftUIStoryTemplate/ContentView.swift
+++ SwiftUIStoryTemplate/ContentView.swift
@@ -5,26 +5,26 @@ struct ContentView: View {
     var body: some View {
         TabView {
             Chapter1View()
-                .tabItem {
-                    Label("Chapter 1", systemImage: "1.circle")
-                }
+//                .tabItem {
+//                    Label("Chapter 1", systemImage: "1.circle")
+//                }
             Chapter2View()
-                .tabItem {
-                    Label("Chapter 2", systemImage: "2.circle")
-                }
+//                .tabItem {
+//                    Label("Chapter 2", systemImage: "2.circle")
+//                }
             Chapter3View()
-                .tabItem {
-                    Label("Chapter 3", systemImage: "3.circle")
-                }
+//                .tabItem {
+//                    Label("Chapter 3", systemImage: "3.circle")
+//                }
             Chapter4View()
-                .tabItem {
-                    Label("Chapter 4", systemImage: "4.circle")
-                }
+//                .tabItem {
+//                    Label("Chapter 4", systemImage: "4.circle")
+//                }
             Chapter5View()
-                .tabItem {
-                    Label("Chapter 5", systemImage: "5.circle")
-                }
-        }
+//                .tabItem {
+//                    Label("Chapter 5", systemImage: "5.circle")
+//                }
+        }.tabViewStyle(.page(indexDisplayMode: .never))
     }
 }
 
