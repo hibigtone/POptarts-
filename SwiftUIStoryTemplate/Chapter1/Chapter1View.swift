e7a54ae6ff1cf8041df8993f910b54f6abcebb2e
Antonio Gregory <antonio.dgregory@gmail.com>
2023-11-17 02:37:47 -0500

    final

--- SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
+++ SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
@@ -2,71 +2,36 @@ import SwiftUI
 
 struct Chapter1View: View {
     let cornerRadius: CGFloat = 10
-    let numOfTransports = 3
+    var numOfTransports = 3
+    @State var buttonPress: Bool = false
+    
     var body: some View {
         
         // 1) make rectangle at top
         ZStack {
             Color
-                .gray
+                .white
                 .ignoresSafeArea()
             ScrollView {
                 VStack {
-                ZStack {
-                    Rectangle()
-                        .foregroundColor(.white)
-                        .frame(width: 365, height: 50)
-                        .cornerRadius(cornerRadius)
-                        .overlay(// MAKES A BORDER ROUNDED
-                            RoundedRectangle(cornerRadius: cornerRadius)
-                                .stroke(.black, lineWidth: 1))
-
-                    HStack {
-                        Text("Chap1")
-                            .font(.title2)
-                            .fontWeight(.semibold)
-                            .padding(.horizontal)
-                            .padding(.leading)
+                    VStack {
+                        Text("Chapter 1")
+                            .font(.system(size: 35,  design: .serif))
+                        Text("Verna")
                         Divider()
-                            .frame(width: 0.5, height: 35)
-                            .overlay(.black)
-
-                        Text("Chap2")
-                            .fontWeight(.light)
-                            .font(.callout)
-                        Divider()
-                            .frame(width: 0.5, height: 35)
-                            .overlay(.black)
-                        Text("Chap3")
-                            .fontWeight(.light)
-                            .font(.callout)
-                        Divider()
-                            .frame(width: 0.35, height: 35)
-                            .overlay(.black)
-                        Text("Chap4")
-                            .fontWeight(.light)
-                            .font(.callout)
-                        Divider()
-                            .frame(width: 0.35, height: 35)
-                            .overlay(.black)
-                        Text("Chap5")
-                            .fontWeight(.light)
-                            .font(.callout)
-                            .padding(.trailing)
                     }
-                }
                 
                 HStack {
                     ZStack {
                         Rectangle()
-                            .foregroundColor(.white)
+                            .foregroundColor(.infobox)
                             .frame(width: 43, height: 305)
                             .cornerRadius(0)
                             .overlay(// MAKES A BORDER ROUNDED
                                 RoundedRectangle(cornerRadius: 0)
                                     .stroke(.black, lineWidth: 1))
                         VStack {
-                            Text("Year: 2000")
+                            Text("Year: 2005")
                                 .rotationEffect(Angle(degrees: 270))
                                 .font(.title3)
                                 .frame(width: 111)
@@ -96,11 +61,11 @@ struct Chapter1View: View {
                     
                     
                     ZStack {
-                        RoundedRectangle(
-                            cornerRadius: 7)
-                        .frame(width: 302, height: 322)
-                        Image("landscape")
+                        Rectangle()
+                            .frame(width: 300, height: 320)
+                        Image("depressed")
                             .resizable()
+                            .scaledToFit()
                             .frame(width: 300, height: 320)
                             .cornerRadius(7)
                            
@@ -110,7 +75,7 @@ struct Chapter1View: View {
                 }
                 ZStack {
                     Rectangle()
-                        .foregroundColor(.white)
+                        .foregroundColor(.textbox)
                         .frame(width: 370)
                         .cornerRadius(7)
                         .overlay(// MAKES A BORDER ROUNDED
@@ -124,10 +89,10 @@ struct Chapter1View: View {
                 } .padding(.vertical)
                     HStack {
                         ZStack {
-                            RoundedRectangle(
-                                cornerRadius: 7)
-                            .frame(width: 302, height: 322)
-                            Image("landscape")
+                            Rectangle()
+                                .frame(width: 300, height: 320)
+                                .cornerRadius(7)
+                            Image("virus")
                                 .resizable()
                                 .frame(width: 300, height: 320)
                                 .cornerRadius(7)
@@ -137,7 +102,7 @@ struct Chapter1View: View {
                         .padding(.leading)
                         ZStack {
                             Rectangle()
-                                .foregroundColor(.white)
+                                .foregroundColor(.infobox)
                                 .frame(width: 43, height: 305)
                                 .cornerRadius(0)
                                 .overlay(// MAKES A BORDER ROUNDED
@@ -174,13 +139,13 @@ struct Chapter1View: View {
                     
                     ZStack {
                         Rectangle()
-                            .foregroundColor(.white)
+                            .foregroundColor(.textbox)
                             .frame(width: 370)
                             .cornerRadius(7)
                             .overlay(// MAKES A BORDER ROUNDED
                                 RoundedRectangle(cornerRadius: 7)
                                     .stroke(.black, lineWidth: 1))
-                        Text("Since her passing I lived life on autopiolt. She would've wanted me to be strong, to live a joyful life in her memory, but instead I buried her memory right underneath my emotions and layed them both to rest alongside my sister. And all it took to dig it all up and bring it back my limelight was one single wrinkled sheet of paper. With all my being wished. I wished she was still here to fill this void inside me, I wished she'd never left me, I wished I could go back. ''What if your wish was reality Kassima.'' ''Who's there?'' I muttered startled due to the random voice calling my name in my own home. ''My name's Verna and I think I can make all your wants into reality.''")
+                        Text("  Since her passing I lived life on autopiolt. She would've wanted me to be strong, to live a joyful life in her memory, but instead I buried her memory right underneath my emotions and layed them both to rest alongside my sister. And all it took to dig it all up and bring it back my limelight was one single wrinkled sheet of paper. With all my being wished. I wished she was still here to fill this void inside me, I wished she'd never left me, I wished I could go back. ''What if your wish was reality Kassima.'' ''Who's there?'' I muttered startled due to the random voice calling my name in my own home. ''My name's Verna and I think I can make all your wants into reality.''")
                             .font(.body)
                             .multilineTextAlignment(.center)
                             .padding()
@@ -190,7 +155,7 @@ struct Chapter1View: View {
                         ZStack {
                            let numOfTransports = 2
                             Rectangle()
-                                .foregroundColor(.white)
+                                .foregroundColor(.infobox)
                                 .frame(width: 43, height: 305)
                                 .cornerRadius(0)
                                 .overlay(// MAKES A BORDER ROUNDED
@@ -227,11 +192,11 @@ struct Chapter1View: View {
                         
                         
                         ZStack {
-                            RoundedRectangle(
-                                cornerRadius: 7)
-                            .frame(width: 302, height: 322)
-                            Image("landscape")
+                            Rectangle()
+                                .frame(width: 300, height: 320)
+                            Image("deal")
                                 .resizable()
+                                .scaledToFit()
                                 .frame(width: 300, height: 320)
                                 .cornerRadius(7)
                                
